232d307d0b657261a9cc71e943a8631311945ba1
Jayrajsinh01 <123796120+Jayrajsinh01@users.noreply.github.com>
2023-01-28 02:06:12 -0500

    modified both the scriptfiles
    -deleted print from script1.py
    -added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 print("Complex is better than complicated.")
 #quote of the day
-print("Flat is better than nested.")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
 print("There should be one-- and preferable only one --obvious way to do it.")
+#lines from zen of python
 print("Although that way may not be obvious at first unless youre dutch.")
