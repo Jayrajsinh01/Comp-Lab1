2bfeb14104632aec52946a2fc67f5b5e529908cf
Jayrajsinh01 <123796120+Jayrajsinh01@users.noreply.github.com>
2023-01-30 01:04:52 -0500

    modified both files in the repo clone
    - Deleted a print() statement from script1.py
    -added a print() statement to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
 print("There should be one-- and preferable only one --obvious way to do it.")
 print("Although that way may not be obvious at first unless youre dutch.")
+print("Namespaces are one honking great ideas -- let's do more of those!")
